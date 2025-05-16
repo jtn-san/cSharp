321d496e24ee2b6beba1f41cc4ffe79eb90712e3
jtn-san <santosjonnathan@aol.com>
2025-05-16 19:34:03 -0300

    aula-01

---
+++ Conceitos-CSharp/Conceitos-CSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+// Tipos númericos 
+byte b = 10;
+int idade = 25;
+long populacao = 7000000000;
+
+
+// Tipos Reais
+float peso = 60.2f;
+double altura = 1.78;
+decimal salario = 2500.99m;
+
+
+// Lógico e texto
+bool aprovado = true;
+char incial = 'P';
+string nome = "P";
+
+// Inferência com var
+
+var cidade = "São Paulo";
+var nota = 8.5;
+var letra = 'A';
+var boleana = true;
+
+Console.WriteLine("Nome: " + nome);
+Console.WriteLine($"Nome: {nome}"); // Interpola
+Console.WriteLine($"Idade: {idade}");
